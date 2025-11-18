f324a9bd3f169f4231a3b66643694431c8340e96
Vishal Sonawane <VSonawane@ap.linedata.com>
2025-11-18 17:02:33 +0530

    finished demo complete

--- release/2.alter_table.sql
+++ release/2.alter_table.sql
@@ -1,3 +1,2 @@
-alter table demo rename column3 to created_date,
-rename column2 to id;alter table demo rename column3 to created_date;
+alter table demo rename column3 to created_date;
 alter table demo rename column2 to id;
