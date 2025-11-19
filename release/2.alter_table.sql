fa50e2540447dc45103ef640fa0077cab0254258
Vishal Sonawane <VSonawane@ap.linedata.com>
2025-11-19 18:52:02 +0530

    push

--- release/2.alter_table.sql
+++ release/2.alter_table.sql
@@ -1,2 +1,2 @@
-alter table SNOWFLAKE_LEARNING_DB.PUBLIC.demo rename column column3 to created_date;
-alter table SNOWFLAKE_LEARNING_DB.PUBLIC.demo rename column column2 to id;
+alter table snow_demo.demo_access_schema.demo rename column column3 to created_date;
+alter table snow_demo.demo_access_schema.demo rename column column2 to id;
