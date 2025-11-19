03ce7070ea40e974873c1562e5e35c28cd278e8d
Vishal Sonawane <VSonawane@ap.linedata.com>
2025-11-19 23:39:06 +0530

    pushed check

--- release/2.alter_table.sql
+++ release/2.alter_table.sql
@@ -1,14 +1,11 @@
-DECLARE 
-    col_exists BOOLEAN;
-
 BEGIN
-    SELECT EXISTS (
-        SELECT 1
-        FROM INFORMATION_SCHEMA.COLUMNS
-        WHERE TABLE_SCHEMA = 'demo_access_schema'
-          AND TABLE_NAME = 'demo'
-          AND COLUMN_NAME = 'column3'
-    ) INTO :col_exists;
+	LET col_exists BOOLEAN := (
+			SELECT COUNT(*) > 0
+			FROM INFORMATION_SCHEMA.COLUMNS
+			WHERE TABLE_SCHEMA = 'demo_access_schema'
+			  AND TABLE_NAME = 'demo'
+			  AND COLUMN_NAME = 'column3'
+		);
 
     IF (col_exists) THEN
         EXECUTE IMMEDIATE 'ALTER TABLE demo_access_schema.demo RENAME COLUMN column3 TO created_date';
@@ -17,13 +14,13 @@ END;
 
 
 BEGIN
-    SELECT EXISTS (
-        SELECT 1
-        FROM INFORMATION_SCHEMA.COLUMNS
-        WHERE TABLE_SCHEMA = 'demo_access_schema'
-          AND TABLE_NAME = 'demo'
-          AND COLUMN_NAME = 'column2'
-    ) INTO :col_exists;
+	LET col_exists BOOLEAN := (
+			SELECT COUNT(*) > 0
+			FROM INFORMATION_SCHEMA.COLUMNS
+			WHERE TABLE_SCHEMA = 'demo_access_schema'
+			  AND TABLE_NAME = 'demo'
+			  AND COLUMN_NAME = 'column2'
+		);
 
     IF (col_exists) THEN
         EXECUTE IMMEDIATE 'ALTER TABLE demo_access_schema.demo RENAME COLUMN column2 TO id';
