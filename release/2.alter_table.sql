bfd4bfac0900fb37ee411a5d47adb8afe682ee42
Vishal Sonawane <VSonawane@ap.linedata.com>
2025-11-19 23:27:52 +0530

    pushed check

--- release/2.alter_table.sql
+++ release/2.alter_table.sql
@@ -1,2 +1,35 @@
-alter table IF EXISTS snow_demo.demo_access_schema.demo rename column column3 to created_date;
-alter table IF EXISTS snow_demo.demo_access_schema.demo rename column column2 to id;
+DECLARE 
+    col_exists BOOLEAN;
+
+BEGINcolumn3
+    SELECT EXISTS (
+        SELECT 1
+        FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE TABLE_SCHEMA = 'demo_access_schema'
+          AND TABLE_NAME = 'demo'
+          AND COLUMN_NAME = 'column3'
+    ) INTO :col_exists;
+
+    IF (col_exists) THEN
+        EXECUTE IMMEDIATE 'ALTER TABLE MY_SCHEMA.MY_TABLE RENAME COLUMN column3 TO created_date';
+    END IF;
+END;
+
+
+DECLARE 
+    col_exists BOOLEAN;
+
+BEGINcolumn3
+    SELECT EXISTS (
+        SELECT 1
+        FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE TABLE_SCHEMA = 'demo_access_schema'
+          AND TABLE_NAME = 'demo'
+          AND COLUMN_NAME = 'column2'
+    ) INTO :col_exists;
+
+    IF (col_exists) THEN
+        EXECUTE IMMEDIATE 'ALTER TABLE MY_SCHEMA.MY_TABLE RENAME COLUMN column2 TO id';
+    END IF;
+END;
+
