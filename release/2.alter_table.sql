cffec7635c1bc670489a8d1a2a0a2d71bf6338d8
Vishal Sonawane <VSonawane@ap.linedata.com>
2025-11-19 23:19:32 +0530

    push

--- release/2.alter_table.sql
+++ release/2.alter_table.sql
@@ -1,2 +1,2 @@
-alter table snow_demo.demo_access_schema.demo rename column column3 to created_date;
-alter table snow_demo.demo_access_schema.demo rename column column2 to id;
+alter table IF EXISTS snow_demo.demo_access_schema.demo rename column column3 to created_date;
+alter table IF EXISTS snow_demo.demo_access_schema.demo rename column column2 to id;
